6eabdcfcc150130688916d817f7d1bdb19bc5853
Natansh05 <natansh.me05@gmail.com>
2024-04-04 23:33:00 +0530

    users acchce vale daal diye hai

---
+++ SQL-Files/insertion_users.sql
@@ -0,0 +1,168 @@
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000001', '9776543210', 'Ramesh Deshmukh', 'ramesh@example.com', 'password123', 'Zone-1, Mumbai, Maharashtra, India', 'ID000000001');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000002', '9123456780', 'Sunita Sharma', 'sunita@example.com', 'password456', 'Area-2, Delhi, Delhi, India', 'ID000000002');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000003', '9777777777', 'Vikram Gowda', 'vikram@example.com', 'password789', 'Street-3, Bangalore, Karnataka, India', 'ID000000003');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000004', '9123456123', 'Deepika Reddy', 'deepika@example.com', 'password246', 'Lane-4, Hyderabad, Telangana, India', 'ID000000004');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000005', '9776543908', 'Rajesh Patel', 'rajesh@example.com', 'password579', 'Plot-5, Ahmedabad, Gujarat, India', 'ID000000005');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000006', '9123456781', 'Anjali Kumar', 'anjali@example.com', 'password135', 'Apartment-6, Chennai, Tamil Nadu, India', 'ID000000006');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000007', '9776543212', 'Amit Banerjee', 'amit@example.com', 'password792', 'Flat-7, Kolkata, West Bengal, India', 'ID000000007');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000008', '9123456709', 'Neha Thakur', 'neha@example.com', 'password468', 'House-8, Pune, Maharashtra, India', 'ID000000008');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000009', '9776543221', 'Prakash Singh', 'prakash@example.com', 'password159', 'Villa-9, Jaipur, Rajasthan, India', 'ID000000009');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000010', '9123456782', 'Priya Shah', 'priya@example.com', 'password357', 'Street-10, Surat, Gujarat, India', 'ID000000010');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000011', '9776543232', 'Arun Verma', 'arun@example.com', 'password111', 'Lane-11, Lucknow, Uttar Pradesh, India', 'ID000000011');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000012', '9123456783', 'Meera Nair', 'meera@example.com', 'password222', 'Apartment-12, Kochi, Kerala, India', 'ID000000012');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000013', '9776543233', 'Vivek Singh', 'vivek@example.com', 'password333', 'Flat-13, Varanasi, Uttar Pradesh, India', 'ID000000013');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000015', '9776543234', 'Sandeep Shah', 'sandeep@example.com', 'password555', 'Villa-15, Nagpur, Maharashtra, India', 'ID000000015');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000016', '9123456785', 'Priyanka Reddy', 'priyanka@example.com', 'password666', 'Street-16,Vishakhapatnam, Andhra Pradesh, India', 'ID000000016');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000017', '9776543235', 'Rahul Mishra', 'rahul@example.com', 'password777', 'Lane-17, Bhopal, Madhya Pradesh, India', 'ID000000017');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000018', '9123456786', 'Neha Kumari', 'neha@example.com', 'password888', 'Plot-18, Patna, Bihar, India', 'ID000000018');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000019', '9776543236', 'Rajiv Singh', 'rajiv@example.com', 'password999', 'Apartment-19, Ludhiana, Punjab, India', 'ID000000019');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000020', '9123456787', 'Aarav Kumar', 'aarav@example.com', 'password123', 'House-20, Mysore, Karnataka, India', 'ID000000020');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000021', '9776543237', 'Ananya Singh', 'ananya@example.com', 'password234', 'Street-21, Jaipur, Rajasthan, India', 'ID000000021');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000022', '9123456788', 'Arjun Das', 'arjun@example.com', 'password345', 'Lane-22, Bhubaneswar, Odisha, India', 'ID000000022');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000023', '9776543238', 'Neha Barua', 'neha@example.com', 'password456', 'Plot-23, Guwahati, Assam, India', 'ID000000023');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000024', '9123456789', 'Rohan Fernandes', 'rohan@example.com', 'password567', 'Apartment-24, Panaji, Goa, India', 'ID000000024');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000025', '9776543239', 'Amit Kumar', 'amit@example.com', 'password678', 'Flat-25, Ranchi, Jharkhand, India', 'ID000000025');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000026', '9123456790', 'Aditi Sharma', 'aditi@example.com', 'password789', 'House-26, Raipur, Chhattisgarh, India', 'ID000000026');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000027', '9776543240', 'Karan Singh', 'karan@example.com', 'password890', 'Street-27, Faridabad, Haryana, India', 'ID000000027');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000028', '9123456791', 'Sonia Verma', 'sonia@example.com', 'password901', 'Lane-28, Dehradun, Uttarakhand, India', 'ID000000028');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000029', '9776543241', 'Rahul Singh', 'rahul@example.com', 'password012', 'Plot-29, Srinagar, Jammu and Kashmir, India', 'ID000000029');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000030', '9123456792', 'Ishaan Choudhury', 'ishaan@example.com', 'password123', 'Apartment-30, Shillong, Meghalaya, India', 'ID000000030');
+
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000031', '9776543242', 'Aryan Kumar', 'aryan@example.com', 'password234', 'Street-31, Patna, Bihar, India', 'ID000000031');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000032', '9123456793', 'Priya Sharma', 'priya@example.com', 'password345', 'Lane-32, Bhopal, Madhya Pradesh, India', 'ID000000032');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000033', '9776543243', 'Vikram Singh', 'vikram@example.com', 'password456', 'Plot-33, Amritsar, Punjab, India', 'ID000000033');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000034', '9123456794', 'Ananya Deb', 'ananya@example.com', 'password567', 'Flat-34, Agartala, Tripura, India', 'ID000000034');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000035', '9776543244', 'Rajesh Singh', 'rajesh@example.com', 'password678', 'House-35, Imphal, Manipur, India', 'ID000000035');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000036', '9123456795', 'Anushka Ralte', 'anushka@example.com', 'password789', 'Street-36, Aizawl, Mizoram, India', 'ID000000036');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000037', '9776543245', 'Amit Gurung', 'amit@example.com', 'password890', 'Lane-37, Gangtok, Sikkim, India', 'ID000000037');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000038', '9123456796', 'Riya Karki', 'riya@example.com', 'password901', 'Plot-38, Itanagar, Arunachal Pradesh, India', 'ID000000038');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000039', '9776543246', 'Suresh Jamir', 'suresh@example.com', 'password012', 'Apartment-39, Kohima, Nagaland, India', 'ID000000039');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000040', '9123456797', 'Aditi Nair', 'aditi@example.com', 'password123', 'Flat-40, Port Blair, Andaman and Nicobar Islands, India', 'ID000000040');
+
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000041', '9776543247', 'Kiran Patel', 'kiran@example.com', 'password234', 'Street-41, Daman, Daman and Diu, India', 'ID000000041');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000042', '9123456798', 'Reena Kumar', 'reena@example.com', 'password345', 'Lane-42, Kavaratti, Lakshadweep, India', 'ID000000042');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000043', '9776543248', 'Rajiv Malhotra', 'rajiv@example.com', 'password456', 'Plot-43, Chandigarh, Chandigarh, India', 'ID000000043');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000044', '9123456799', 'Sanjay Mehta', 'sanjay@example.com', 'password567', 'Flat-44, Silvassa, Dadra and Nagar Haveli, India', 'ID000000044');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000045', '9776543249', 'Meera Das', 'meera@example.com', 'password678', 'House-45, Shillong, Meghalaya, India', 'ID000000045');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000046', '9123456700', 'Rohan Naik', 'rohan@example.com', 'password789', 'Street-46, Panaji, Goa, India', 'ID000000046');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000047', '9776543250', 'Ashish Singh', 'ashish@example.com', 'password890', 'Lane-47, Bhopal, Madhya Pradesh, India', 'ID000000047');
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000048', '9123456701', 'Nisha Yadav', 'nisha@example.com', 'password901', 'Plot-48, Jaipur, Rajasthan, India', 'ID000000048');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000049', '9776543251', 'Kunal Sharma', 'kunal@example.com', 'password012', 'Apartment-49, Mumbai, Maharashtra, India', 'ID000000049');
+
+
+INSERT INTO Users (User_ID, Phone_Number, Full_Name, Email, Password, Address, Government_ID)
+VALUES ('USR0000050', '9123456702', 'Ananya Gupta', 'ananya@example.com', 'password123', 'Flat-50, Kolkata, West Bengal, India', 'ID000000050');
