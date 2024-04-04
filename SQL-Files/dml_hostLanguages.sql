e63147653c61150f493e12cbd460bc0ce0c49846
natansh05 <202201445@daiict.ac.in>
2024-04-04 19:47:52 +0530

    Create dml_hostLanguages.sql

---
+++ SQL-Files/dml_hostLanguages.sql
@@ -0,0 +1,31 @@
+-- Insert sample data into Host_Languages table for each host
+
+-- host languages insertion query for hosts specified
+
+INSERT INTO Host_Languages (Host_ID, Languages_Spoken)
+VALUES 
+('HST0000001', 'English, Hindi'),
+('HST0000002', 'English, Hindi'),
+('HST0000003', 'English, Gujarati'),
+('HST0000004', 'English, Marathi'),
+('HST0000005', 'English, Kannada'),
+('HST0000006', 'English, Punjabi'),
+('HST0000007', 'English, Telugu'),
+('HST0000008', 'English, Tamil'),
+('HST0000009', 'English, Bengali'),
+('HST0000010', 'English, Malayalam'),
+('HST0000011', 'English, Marathi'),
+('HST0000012', 'English, Gujarati'),
+('HST0000013', 'English, Hindi'),
+('HST0000014', 'English, Punjabi'),
+('HST0000015', 'English, Telugu'),
+('HST0000016', 'English, Tamil'),
+('HST0000017', 'English, Bengali'),
+('HST0000018', 'English, Malayalam'),
+('HST0000019', 'English, Kannada'),
+('HST0000020', 'English, Hindi'),
+('HST0000021', 'English, Marathi'),
+('HST0000022', 'English, Gujarati'),
+('HST0000023', 'English, Telugu'),
+('HST0000024', 'English, Tamil'),
+('HST0000025', 'English, Bengali');
