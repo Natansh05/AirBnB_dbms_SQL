b6275b1811f75922338f925f762f6da8689fcbc4
natansh05 <202201445@daiict.ac.in>
2024-04-19 02:41:55 +0530

    Add files via upload

---
+++ SQL-Files/insertion_preferences.sql
@@ -0,0 +1,35 @@
+
+INSERT INTO Preferences (User_ID, Pref_Aminities, Pref_Property_Type, Pref_Location) VALUES
+('USR0000001', 'Free WiFi', 'Condo', 'Mumbai'),
+('USR0000002', 'Pool', 'Villa', 'Delhi'),
+('USR0000004', 'Parking', 'Apartment', 'Hyderabad'),
+('USR0000005', 'Pet Friendly', 'House', 'Ahmedabad'),
+('USR0000006', 'Free WiFi', 'Apartment', 'Chennai'),
+('USR0000008', 'Pool', 'Villa', 'Pune'),
+('USR0000009', 'Air Conditioning', 'House', 'Jaipur');
+
+-- Continuing with more examples
+INSERT INTO Preferences (User_ID, Pref_Aminities, Pref_Property_Type, Pref_Location) VALUES
+('USR0000011', 'Free WiFi', 'Room', 'Lucknow'),
+('USR0000013', 'Air Conditioning', 'Room', 'Varanasi'),
+('USR0000015', 'Pet Friendly', 'Apartment', 'Nagpur'),
+('USR0000016', 'Free WiFi', 'Condo', 'Vishakhapatnam'),
+('USR0000019', 'Gym', 'Room', 'Ludhiana');
+
+-- -- delete from preferences where Pref_Property_Type = 'Bungalow';
+
+
+INSERT INTO Preferences (User_ID, Pref_Aminities, Pref_Property_Type, Pref_Location) VALUES
+('USR0000026', 'Air Conditioning', 'Villa', 'Raipur'),
+('USR0000027', 'Gym', 'Apartment', 'Faridabad'),
+('USR0000029', 'Pool', 'House', 'Srinagar'),
+('USR0000030', 'Free WiFi', 'Condo', 'Shillong'),
+('USR0000032', 'Garden', 'Room', 'Bhopal'),
+('USR0000034', 'Gym', 'Condo', 'Agartala'),
+('USR0000035', 'Parking', 'Apartment', 'Imphal'),
+('USR0000036', 'Free WiFi', 'Villa', 'Aizawl'),
+('USR0000038', 'Pool', 'House', 'Itanagar'),
+('USR0000039', 'Air Conditioning', 'Condo', 'Kohima');
+
+-- -- delete from preferences;
+-- select * from preferences
